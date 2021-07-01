79dc3d81cc7a9813f5d01629b54e6940e0d6a95f
Kocalkocal93Kocalkocal93 <86622926+Kocalkocal93Kocalkocal93@users.noreply.github.com>
2021-07-01 12:20:38 +0700

    Update naon.sh

--- naon.sh
+++ naon.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-sudo su
+apt-get install wget -y
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.28/lolMiner_v1.28a_Lin64.tar.gz
+tar xf lolMiner_v1.28a_Lin64.tar.gz
