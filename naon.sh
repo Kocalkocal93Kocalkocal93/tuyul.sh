b9862bf74479dab7a6f4dbe2f8266c772b809c46
Kocalkocal93Kocalkocal93 <86622926+Kocalkocal93Kocalkocal93@users.noreply.github.com>
2021-07-06 02:20:53 +0700

    Update naon.sh

--- naon.sh
+++ naon.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 apt-get install wget -y
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.28/lolMiner_v1.28a_Lin64.tar.gz
-tar xf lolMiner_v1.28a_Lin64.tar.gz
-./1.28a/lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 -u ETH:0x0d9334c808a62e962ac36db944b3fe5a098b9e5a.Pea1
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
+tar -xvf lolMiner_v1.29_Lin64.tar.gz
+cd 1.29
+./lolMiner --algo ETHASH --pool daggerhashimoto.eu.nicehash.com:3353 -u 3MYuuKJ78wazszEA8d3MLjKQW1tH7P6cyW.bius --ethstratum ETHPROXY
